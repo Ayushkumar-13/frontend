7c0c2b9fee5c5ba80abe6b2f810af51694bd6916
Ayush Kumar <ayushkumar6064@gmail.com>
2025-03-12 23:18:06 +0530

    day1_JS

---
+++ JavaScript/day1/script.js
@@ -0,0 +1,23 @@
+// // console.log("Hello world");
+// function fn(){
+// // alert("Hello world");
+// setTimeout(function() {
+//     console.log("settimeout1");
+// }, 2000);
+// setTimeout(() => {
+//     console.log("settimeout3");
+// },2000);
+
+// setTimeout(() => {
+//     console.log("settimeout 2");
+// },3000);
+// console.log("helloworld");
+
+// }
+
+// function fn () {
+// console.log("hey");
+// }
+let fn = () => {
+   console.log("Hello World");
+}
