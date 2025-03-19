eef18fd0ce8ffd729a0ae0090e418e4e28d6bd90
Ayush Kumar <ayushkumar6064@gmail.com>
2025-03-19 23:55:29 +0530

    added day2

---
+++ JavaScript/day2/script.js
@@ -0,0 +1,70 @@
+// console.log("hello!!!!!!!!")
+
+// -------------------- var let const 
+
+// var a = 10;
+// var b = "hello";
+// var c = {};
+// var d = [1,2,3,4,5];
+// var e = function(a,b) {return a + b};
+// a = "hello";
+// // console.log(e(2,5));
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+// console.log(typeof a)
+// -------------------------------------------------------------
+// console.log(fn(3,5)); 
+                                //   thi is the concept of hoisting
+// function fn(a,b){
+//     return a + b;
+// }
+// -------------------------------------------------------------------------
+
+
+// var fn = function (a,b) {
+//     return a + b;
+// }
+// console.log(fn(3,3));
+
+// var a = 10;
+// console.log(a) ;   output 10
+
+
+// console.log(a) ;  output undefined
+// var a = 10;
+
+// var a = 10
+// console.log(a,b);
+// var b = 20;
+// var a = 10;
+// var b = 20;
+// var fn = function(a,b) {return a + b};
+// fn(3,3);
+
+// var x = 1;
+// f1();
+
+// function f1(){
+//         var x = 10;
+//         x = 10;
+//         f2();
+//         // console.log(x)
+
+// }
+// function f2(){
+//     var x = 20;
+//     console.log(x);
+
+// }
+// f1();
+
+// var x = 1;
+// var x = 10;
+
+// console.log(x)
+let x = 1
+ x= 20;
+console.log(x)
