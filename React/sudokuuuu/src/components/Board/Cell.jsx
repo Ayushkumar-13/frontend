fc8d0952ab5c46f8805b3fc4b0a1b49787b127cd
Ayush Kumar <ayushkumar6064@gmail.com>
2025-05-24 16:37:19 +0530

    added 1-55-00

--- React/sudokuuuu/src/components/Board/Cell.jsx
+++ React/sudokuuuu/src/components/Board/Cell.jsx
@@ -12,6 +12,8 @@ function Cell({ row, col }) {
   function isSelected() {
     const query = { current: false, other: false }
     if (selectedCell.row == row && selectedCell.col == col) query.current = true
+    if (selectedCell.row == row || selectedCell.col == col) query.other = true
+    if ( qBoard[row][col].value != 0 && qBoard[row][col].value == qBoard[selectedCell.row][selectedCell.col].value) query.other = true
     return query
   }
 
@@ -21,6 +23,9 @@ function Cell({ row, col }) {
   className={`Cell select-none flex items-center justify-center cursor-pointer w-full h-full rounded-md 
     ${isSelected().current 
       ? "bg-slate-950 outline outline-1 outline-blue-500" 
+      : "bg-slate-800 hover:outline hover:outline-1 hover:outline-white"}
+      ${isSelected().other
+      ? "bg-slate-900 " 
       : "bg-slate-800 hover:outline hover:outline-1 hover:outline-white"}`}
 >
   {qBoard[row][col].value !== 0 && (
