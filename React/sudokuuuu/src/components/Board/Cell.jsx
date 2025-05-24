b726e9b2be5508658f2c536373dc049311007736
Ayush Kumar <ayushkumar6064@gmail.com>
2025-05-24 06:27:59 +0530

    no-generated-with-empty-space

--- React/sudokuuuu/src/components/Board/Cell.jsx
+++ React/sudokuuuu/src/components/Board/Cell.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useGame } from '../../store/gameStore'
 
-function Cell({row,col}) {
-    
+function Cell({ row, col }) {
+ const {qBoard} = useGame()
   return (
-    <div onClick={()=> console.log(row,col)} className='Cell select-none flex items-center justify-center cursor-pointer bg-slate-800 w-full h-full rounded-md hover:outline hover:outline-[1px]'>
-
+    <div className='Cell select-none flex items-center justify-center cursor-pointer bg-slate-800 w-full h-full rounded-md hover:outline hover:outline-[1px]'>
+      { qBoard[row][col].value !=0 && <span className='text-2xl md:text-3xl'> {qBoard[row][col].value} </span>}
     </div>
   )
 }
